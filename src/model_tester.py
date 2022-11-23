6cbe1ec2bab31246df2bd3067bb922a7a0731351
Sven <svenh91@gmail.com>
2022-11-23 16:35:39 +0000

    test on seperate test set, print learn rate

--- src/model_tester.py
+++ src/model_tester.py
@@ -1,39 +1,23 @@
 import torch
 from dataset import GTZAN
-import sys
-# import torch.nn.functional as F
+import sys, os
+import os
+
 import torch.nn as nn
 
 from model import ShallowMusicCNN
 dataset = GTZAN('../data/train.pkl')
-filename, spectrogram, label, samples = dataset[0] 
+dataset_test = GTZAN('../data/val.pkl')
+print(len(dataset_test))
+batcher = dataset.get_batches(batch_size=20, num_batches=None, random_seed=42)
 
-
-
-num_classes = 2
+# get 100 test samples
+batcher_test = dataset.get_batches(batch_size=20, num_batches=None, random_seed=42)
+for spectrograms_test, labels_test in dataset_test.get_batches(batch_size=500, num_batches=None, random_seed=42):
+    break
+print('size of test set:',spectrograms_test.shape[0])
+num_classes = 10
 model = ShallowMusicCNN(num_classes)
-# model
-
-
-# def show_model_weights(model):
-#     layers = list(model.modules())[0]
-#     layers = model.modules()
-#     print(layers)
-#     for layer in layers:
-#         print('='*20)
-#         print(type(layer), layer)
-#         if hasattr(layer, "weight"):
-#             print(layer.weight)
-#         else:
-#             print('no weight')
-
-
-
-# spectrograms = torch.stack([spectrogram for filename, spectrogram, label, samples in dataset])
-
-# # convert label to one hot (cat instead of stack because don't want to add extra dimension)
-# label_class = torch.LongTensor([label % num_classes for filename, spectrogram, label, samples in dataset])
-# labels = nn.functional.one_hot(label_class, num_classes=num_classes).float()
 
 
 import numpy as np
@@ -41,11 +25,14 @@ from torchmetrics import Accuracy
 
 
 
-criterion = nn.CrossEntropyLoss()
+# utility for getting the current learning rate
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+        
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0005, betas=(0.9, 0.999), eps=1e-08)
 # optimizer = torch.optim.SGD(model.parameters(), lr=0.05, momentum=0.9)
 accuracy = Accuracy()
-batcher = dataset.get_batches(batch_size=20, num_batches=None, random_seed=42)
 
 # for epoch in range(1000):
 for epoch, (spectrograms, labels) in enumerate(batcher):
@@ -57,7 +44,7 @@ for epoch, (spectrograms, labels) in enumerate(batcher):
     pred_class = preds.argmax(dim=1)
     label_class = labels.argmax(dim=1)
     # print(pred_class)
-    loss = criterion(preds, labels)
+    loss = F.cross_entropy(preds, labels)
 
     loss.backward()
     # update weights
@@ -69,10 +56,29 @@ for epoch, (spectrograms, labels) in enumerate(batcher):
     # print(model.fc2.weight[0, 0:4],accuracy(pred_class.int(), label_class.int()))
     # values of weight tensors
     wvis = (
-        model.fc1.weight[0, 0:4].detach().numpy(),
-        model.fc2.weight[0, 0:4].detach().numpy(),
+        model.fc1.weight[0, 0:2].detach().numpy(),
+        model.fc2.weight[0, 0:2].detach().numpy(),
         model.conv1.weight[0, 0, 0, 0:1].detach().numpy(),
         model.conv2.weight[0, 0, 0, 0:1].detach().numpy(),
     )
-    print(epoch, accuracy(pred_class.int(), label_class.int()).detach().numpy(), torch.round(loss,decimals=3).detach().numpy(), torch.round(preds.sum(dim=0),decimals=3).detach().numpy(),*wvis)
+    if epoch > 100:
+        # print('stop showing weights')
+        wvis = ''
+    print(epoch,
+        get_lr(optimizer),
+        accuracy(pred_class.int(), label_class.int()).detach().numpy(),
+        pred_class.int().detach().numpy(),
+        torch.round(loss, decimals=3).detach().numpy(),
+        # torch.round(preds.sum(dim=0),decimals=3).detach().numpy(),
+            *wvis)
+    if epoch % 100 == 0:
+        print('test', '='*20)
+        with torch.no_grad():
+            output = model(spectrograms_test)
+            preds = output
+            pred_class = preds.argmax(dim=1)
+            label_class = labels_test.argmax(dim=1)
+            print(epoch,
+                get_lr(optimizer),
+                accuracy(pred_class.int(), label_class.int()).detach().numpy())
 # show_model_weights(model)
