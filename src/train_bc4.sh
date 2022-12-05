fc006366b535ca573aaa86080fd0b6b3717b4774
Sven <svenh91@gmail.com>
2022-12-05 13:22:43 +0000

    Create train_bc4.sh

---
+++ src/train_bc4.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=lab2
+#SBATCH --partition=teach_gpu
+#SBATCH --nodes=1
+#SBATCH -o ./log_%j.out # STDOUT out
+#SBATCH -e ./log_%j.err # STDERR out
+#SBATCH --gres=gpu:1
+#SBATCH --time=0:20:00
+#SBATCH --mem=4GB
+
+# get rid of any modules already loaded
+module purge
+# load in the module dependencies for this script
+module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
+conda activate sven
+
+python main.py
