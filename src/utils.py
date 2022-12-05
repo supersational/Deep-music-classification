d35d6ee334b9ebc6ec58e68d9a01be5dfdcf0df4
neutralpronoun <alexowendavies@gmail.com>
2022-12-05 14:14:40 +0000

    Added confusion matrix

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,15 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
+
+def plot_confusion_matrix(y_true, y_pred, tag = ''):
+    fig, ax = plt.subplots(figsize = (6,6))
+
+    ConfusionMatrixDisplay.from_predictions(y_true, y_pred, ax = ax)
+
+    plt.tight_layout()
+    plt.savefig(f'../results/confusion{tag}.png')
 
 def get_batch_ids(N, batch_size):
     sample_ids = np.arange(N)
@@ -18,17 +27,17 @@ def get_batch_ids(N, batch_size):
         batch_ids.append(batch_i)
     return batch_ids
 
-def plot_losses(train_loss, val_loss):
+def plot_losses(train_loss, val_loss, tag = ''):
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
     axes[0].plot((train_loss), label='train')
     axes[1].plot((val_loss), label='test')
     axes[0].legend()
     axes[1].legend()
 
-    plt.savefig('../results/losses.png')
+    plt.savefig(f'../results/losses{tag}.png')
     plt.close()
 
-def plot_accuracies(train_accuracies, val_accuracies):
+def plot_accuracies(train_accuracies, val_accuracies, tag = ''):
     plt.plot(train_accuracies, label='Train')
     plt.plot(val_accuracies, label='Test')
     plt.ylabel('Accuracy')
@@ -37,5 +46,5 @@ def plot_accuracies(train_accuracies, val_accuracies):
     print('Train Acc: ', train_accuracies[-1])
     print('Test Acc: ', val_accuracies[-1])
 
-    plt.savefig('../results/accuracies.png')
+    plt.savefig(f'../results/accuracies{tag}.png')
     plt.close()
