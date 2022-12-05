08d7328c6ebeb042458dbb79f5b7dc313902f2a8
Sven <svenh91@gmail.com>
2022-12-05 16:15:05 +0000

    fix sbatch logs

--- src/train_bc4.sh
+++ src/train_bc4.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=lab2
+#SBATCH --job-name=lab4
 #SBATCH --partition=teach_gpu
 #SBATCH --nodes=1
-#SBATCH -o ./log_%j.out # STDOUT out
-#SBATCH -e ./log_%j.err # STDERR out
+#SBATCH -o ./slogs/log_bc4_%j.out # STDOUT out
+#SBATCH -e ./slogs/log_bc4_%j.err # STDERR out
 #SBATCH --gres=gpu:1
-#SBATCH --time=0:20:00
-#SBATCH --mem=4GB
+#SBATCH --time=0:01:00
+#SBATCH --mem=16GB
 
 # get rid of any modules already loaded
 module purge
 # load in the module dependencies for this script
 module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
-conda activate sven
+# conda activate sven
 
-python main.py
+python -u main.py >> "./slogs/log_bc4_$SLURM_JOB_ID.txt"
+
+# find /user/home/sh14603/Deep-music-classification/src/ -type f -mmin -10 -exec tail -f {} +
