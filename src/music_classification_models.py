dd2b081d88e288ef9ab5e6200834ad6a3f2f3aa4
Sven <svenh91@gmail.com>
2022-12-06 13:30:00 +0000

    refactor intialise_layer method

--- src/music_classification_models.py
+++ src/music_classification_models.py
@@ -3,6 +3,14 @@ from torch import nn
 from torch.nn import functional as F
 
 
+def initialise_layer(layer):
+    if hasattr(layer, "bias"):
+        nn.init.zeros_(layer.bias)
+    if hasattr(layer, "weight"):
+        # nn.init.xavier_uniform_(layer.weight)
+        nn.init.kaiming_normal_(layer.weight)
+
+
 class ShallowMusicCNN(nn.Module):
     def __init__(self, class_count: int, disable_half=False):
         super().__init__()
@@ -17,7 +25,7 @@ class ShallowMusicCNN(nn.Module):
             padding='same'
         )
 
-        self.initialise_layer(self.conv1)
+        initialise_layer(self.conv1)
         self.pool1 = nn.MaxPool2d(kernel_size=(1, 20))
 
         self.conv2 = nn.Conv2d(
@@ -26,22 +34,22 @@ class ShallowMusicCNN(nn.Module):
             kernel_size=(21, 20),
             padding='same',
         )
-        self.initialise_layer(self.conv2)
+        initialise_layer(self.conv2)
         self.pool2 = nn.MaxPool2d(kernel_size=(20, 1))
 
         self.leakyRelu = nn.LeakyReLU(
             negative_slope=0.3)  # need alpha = 0.3 here.. not sure if that's the same as negative slope
-        self.initialise_layer(self.leakyRelu)
+        initialise_layer(self.leakyRelu)
 
         # fully connected map 1x10240 to 200
         if self.disable_half:
             self.fc1 = nn.Linear(int(10240 / 2), 200)
         else:
             self.fc1 = nn.Linear(10240, 200)
-        self.initialise_layer(self.fc1)
+        initialise_layer(self.fc1)
         # final layer (not specified in paper)
         self.fc2 = nn.Linear(200, self.class_count)
-        self.initialise_layer(self.fc2)
+        initialise_layer(self.fc2)
 
     def forward(self, spectograms: torch.Tensor, DEBUG=False) -> torch.Tensor:
         if DEBUG: print('input shape', spectograms.shape)
@@ -94,13 +102,6 @@ class ShallowMusicCNN(nn.Module):
 
         return x
 
-    @staticmethod
-    def initialise_layer(layer):
-        if hasattr(layer, "bias"):
-            nn.init.zeros_(layer.bias)
-        if hasattr(layer, "weight"):
-            # nn.init.xavier_uniform_(layer.weight)
-            nn.init.kaiming_normal_(layer.weight)
 
 
 class DeepMusicCNN(nn.Module):
@@ -116,9 +117,9 @@ class DeepMusicCNN(nn.Module):
             in_channels=self.input_shape[2], out_channels=16, kernel_size=(21, 20),
             padding='same')
 
-        self.initialise_layer(self.conv11)
+        initialise_layer(self.conv11)
         self.pool11 = nn.MaxPool2d(kernel_size=(2, 2))  # Check stride
-        self.initialise_layer(self.conv12)
+        initialise_layer(self.conv12)
         self.pool12 = nn.MaxPool2d(kernel_size=(2, 2))
 
         self.conv21 = nn.Conv2d(
@@ -128,9 +129,9 @@ class DeepMusicCNN(nn.Module):
             in_channels=16, out_channels=32, kernel_size=(10, 5),
             padding='same')
 
-        self.initialise_layer(self.conv21)
+        initialise_layer(self.conv21)
         self.pool21 = nn.MaxPool2d(kernel_size=(2, 2))  # Check stride
-        self.initialise_layer(self.conv22)
+        initialise_layer(self.conv22)
         self.pool22 = nn.MaxPool2d(kernel_size=(2, 2))
 
         self.conv31 = nn.Conv2d(
@@ -140,9 +141,9 @@ class DeepMusicCNN(nn.Module):
             in_channels=32, out_channels=64, kernel_size=(5, 3),
             padding='same')
 
-        self.initialise_layer(self.conv31)
+        initialise_layer(self.conv31)
         self.pool31 = nn.MaxPool2d(kernel_size=(2, 2))  # Check stride
-        self.initialise_layer(self.conv32)
+        initialise_layer(self.conv32)
         self.pool32 = nn.MaxPool2d(kernel_size=(2, 2))
 
         self.conv41 = nn.Conv2d(
@@ -152,15 +153,15 @@ class DeepMusicCNN(nn.Module):
             in_channels=64, out_channels=128, kernel_size=(4, 2),
             padding='same')
 
-        self.initialise_layer(self.conv41)
+        initialise_layer(self.conv41)
         self.pool41 = nn.MaxPool2d(kernel_size=(1, 5))  # Check stride
-        self.initialise_layer(self.conv42)
+        initialise_layer(self.conv42)
         self.pool42 = nn.MaxPool2d(kernel_size=(5, 1))
 
         self.fc1 = nn.Linear(5120, 200)
-        self.initialise_layer(self.fc1)
+        initialise_layer(self.fc1)
         self.fc2 = nn.Linear(200, 10)
-        self.initialise_layer(self.fc2)
+        initialise_layer(self.fc2)
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
         x_left, x_right = F.relu(self.conv11(images)), F.relu(self.conv12(images))
@@ -183,12 +184,6 @@ class DeepMusicCNN(nn.Module):
         x_conc = self.fc2(x_conc)
         return x_conc
 
-    @staticmethod
-    def initialise_layer(layer):
-        if hasattr(layer, "bias"):
-            nn.init.zeros_(layer.bias)
-        if hasattr(layer, "weight"):
-            nn.init.kaiming_normal_(layer.weight)
 
 class FilterMusicCNN(nn.Module):
     "3-branch model with each branch applying a different range of frequency-filter"
@@ -208,11 +203,11 @@ class FilterMusicCNN(nn.Module):
             in_channels=self.input_shape[2], out_channels=16, kernel_size=(21, 20),
             padding='same')
 
-        self.initialise_layer(self.convhigh1)
+        initialise_layer(self.convhigh1)
         self.poolhigh1 = nn.MaxPool2d(kernel_size=(2, 2))  # Check stride
-        self.initialise_layer(self.convmid1)
+        initialise_layer(self.convmid1)
         self.poolmid1 = nn.MaxPool2d(kernel_size=(2, 2))
-        self.initialise_layer(self.convlow1)
+        initialise_layer(self.convlow1)
         self.poollow1 = nn.MaxPool2d(kernel_size=(2, 2))
         
         #================================
@@ -227,11 +222,11 @@ class FilterMusicCNN(nn.Module):
             in_channels=16, out_channels=32, kernel_size=(5, 11),
             padding='same')
 
-        self.initialise_layer(self.convhigh2)
+        initialise_layer(self.convhigh2)
         self.poolhigh2 = nn.MaxPool2d(kernel_size=(2, 2))  # Check stride
-        self.initialise_layer(self.convmid2)
+        initialise_layer(self.convmid2)
         self.poolmid2 = nn.MaxPool2d(kernel_size=(2, 2))
-        self.initialise_layer(self.convlow2)
+        initialise_layer(self.convlow2)
         self.poollow2 = nn.MaxPool2d(kernel_size=(2, 2))
         
         #==========================
@@ -246,11 +241,11 @@ class FilterMusicCNN(nn.Module):
             in_channels=32, out_channels=64, kernel_size=(3, 5),
             padding='same')
 
-        self.initialise_layer(self.convhigh3)
+        initialise_layer(self.convhigh3)
         self.poolhigh3 = nn.MaxPool2d(kernel_size=(2, 2))  # Check stride
-        self.initialise_layer(self.convmid3)
+        initialise_layer(self.convmid3)
         self.poolmid3 = nn.MaxPool2d(kernel_size=(2, 2))
-        self.initialise_layer(self.convlow3)
+        initialise_layer(self.convlow3)
         self.poollow3 = nn.MaxPool2d(kernel_size=(2, 2))
         
         #==========================
@@ -265,20 +260,20 @@ class FilterMusicCNN(nn.Module):
             in_channels=64, out_channels=128, kernel_size=(2, 4),
             padding='same')
 
-        self.initialise_layer(self.convhigh4)
+        initialise_layer(self.convhigh4)
         self.poolhigh4 = nn.MaxPool2d(kernel_size=(1, 5))  # Check stride
-        self.initialise_layer(self.convmid4)
+        initialise_layer(self.convmid4)
         self.poolmid4 = nn.MaxPool2d(kernel_size=(1, 5))
-        self.initialise_layer(self.convlow4)
+        initialise_layer(self.convlow4)
         self.poollow4 = nn.MaxPool2d(kernel_size=(1, 5))
         
         
         #==========================
         # self.fc1 = nn.Linear(5120, 200)
         self.fc1 = nn.Linear(7680, 200)
-        self.initialise_layer(self.fc1)
+        initialise_layer(self.fc1)
         self.fc2 = nn.Linear(200, 10)
-        self.initialise_layer(self.fc2)
+        initialise_layer(self.fc2)
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
 
@@ -287,7 +282,7 @@ class FilterMusicCNN(nn.Module):
 
         x_mid  = torch.clone(images)
         x_mid[:, self.filter_dim:-self.filter_dim, :] = 0.
-        
+
         x_high = torch.clone(images)
         x_high[:, :-self.filter_dim, :] = 0
 
@@ -314,11 +309,3 @@ class FilterMusicCNN(nn.Module):
         x_conc = F.relu(self.fc1(x_conc))
         x_conc = self.fc2(x_conc)
         return x_conc
-
-    @staticmethod
-    def initialise_layer(layer):
-        if hasattr(layer, "bias"):
-            nn.init.zeros_(layer.bias)
-        if hasattr(layer, "weight"):
-            nn.init.kaiming_normal_(layer.weight)
-
