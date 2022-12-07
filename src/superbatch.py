9ad1df5f07b67bc7fd7adac9fcddf390a9df9a8d
Sven <svenh91@gmail.com>
2022-12-07 22:49:02 +0000

    superbatch.py

--- src/superbatch.py
+++ src/superbatch.py
@@ -1,18 +1,25 @@
 import os
 import re
 import time
-model = 'shallow'
-for run_n in range(1,6):
-    for run_type in [1,4]:
-        print('run ',run_n)
-        print('run_type',run_type)
-        with open('train_auto.sh','w') as f:
-            if run_type == 1:
-                cmd = f'python -u main.py --epochs 200 --batch_size 4096 --tag default --model {model} --run_n {run_type}{run_n} >> "./slogs/log_bc4_$SLURM_JOB_ID.txt"'
-            else:
-                cmd = f'python -u main.py --epochs 200 --batch_size 4096 --dropout 0.0 --alpha 0.0 --tag ndnanl1 --l1 0.0  --model {model} --run_n {run_type}{run_n} >> "./slogs/log_bc4_$SLURM_JOB_ID.txt"'
 
-            f.write(f"""#!/usr/bin/env bash
+# for model in ['shallow','deep']:
+for model in ['filter']:
+    MINUTES = 10 if model == 'shallow' else 20
+    BATCH_SIZE = 4096
+    if model == 'filter':
+        BATCH_SIZE = 2048
+        MINUTES = 30
+    for run_n in range(1,6):
+        for run_type in [1,4]:
+            print('run ',run_n)
+            print('run_type',run_type)
+            with open('train_auto.sh','w') as f:
+                if run_type == 1:
+                    cmd = f'python -u main.py --epochs 200 --batch_size {BATCH_SIZE} --tag default --model {model} --run_n {run_type}{run_n} >> "./slogs/log_bc4_$SLURM_JOB_ID.txt"'
+                else:
+                    cmd = f'python -u main.py --epochs 200 --batch_size {BATCH_SIZE} --dropout 0.0 --alpha 0.0 --tag ndnanl1 --l1 0.0  --model {model} --run_n {run_type}{run_n} >> "./slogs/log_bc4_$SLURM_JOB_ID.txt"'
+
+                f.write(f"""#!/usr/bin/env bash
 
 #SBATCH --job-name=lab4
 #SBATCH --partition=teach_gpu
@@ -20,7 +27,7 @@ for run_n in range(1,6):
 #SBATCH -o ./slogs/log_bc4_%j.out # STDOUT out
 #SBATCH -e ./slogs/log_bc4_%j.err # STDERR out
 #SBATCH --gres=gpu:1
-#SBATCH --time=0:10:00
+#SBATCH --time=0:{MINUTES}:00
 #SBATCH --mem=16GB
 
 # get rid of any modules already loaded
@@ -28,9 +35,9 @@ module purge
 # load in the module dependencies for this script
 module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
 {cmd}
-""")
+    """)
 
-        txt = os.popen('sbatch train_auto.sh').read()
-        print(txt)
-        print('='*100)
-        time.sleep(0.5)
+            txt = os.popen('sbatch train_auto.sh').read()
+            print(txt)
+            print('='*100)
+            time.sleep(0.5)
